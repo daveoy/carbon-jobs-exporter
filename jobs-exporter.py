84d7c364b0992f08c221cfa1d26f676c5aff4e5b
Dave Young <dave.andrew.young@gmail.com>
2023-07-06 13:22:02 -0500

    script + skaffold for local dev

---
+++ jobs-exporter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import time
+import subprocess as sp
+from prometheus_client import start_http_server, Gauge
+
+class Jobs:
+    def __init__(self):
+        self.projects_path = os.environ.get('JOBS_PATH', '/mnt/flame_archive')
+        self.projects_list = [ x for x in os.listdir(self.projects_path)]
+        self.projects = self.get_projects()
+    def get_project_size(self,project_path):
+        cmd = ['/bin/du','-s',project_path]
+        runit = sp.Popen(cmd,stdout=sp.PIPE,stderr=sp.PIPE)
+        out,err = runit.communicate()
+        return out.decode().strip().split('\t')[0]
+    def get_projects(self):
+        return [Project(name=project,size=self.get_project_size(os.path.join(self.projects_path,project)),path=os.path.join(self.projects_path,project)) for project in self.projects_list]
+
+class Project:
+    def __init__(self,name,size,path):
+        self.name = name
+        self.size = size
+        self.path = path
+        self.collect()
+    def collect(self):
+        try:
+            projectsizemetric.labels(self.path,self.name).set(self.size)
+        except:
+            print(self.__dict__)
+
+# if __name__ == "__main__":
+#     import time
+#     while True:
+#         print('generating metrics')
+#         st = time.time()
+#         jobs = Jobs()
+#         jobs.write_prometheus_metrics()
+#         et = time.time()
+#         elapsed_time = et - st
+#         print('Execution time:', elapsed_time, 'seconds')
+
+projectsizemetric = Gauge('carbon_vfx_projects', 'VFX project directory size',labelnames=['path','projectname'])
+start_http_server(int(os.environ.get('EXPORTER_PORT',9100)))
+while True:
+    jobs = Jobs()
+    time.sleep(30)
